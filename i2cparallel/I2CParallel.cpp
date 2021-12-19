1ae9a3e7b960f0969ddf60a59e4be7074a847588
Aaron Kimball <akimball83@gmail.com>
2021-12-18 20:24:15 -0800

    Bugfix: LCD now properly moves cursor between screens on newline or line wrap as needed.
    A `\n` on line 2 (or line overrun into line 3) did not properly move the
    cursor blink/visibility state from the upper subscreen to the lower
    subscreen. This is now fixed.

--- i2cparallel/I2CParallel.cpp
+++ i2cparallel/I2CParallel.cpp
@@ -78,6 +78,6 @@ void I2CParallel::increment() {
 
 void I2CParallel::waitForValid() {
   delayMicroseconds(I2C_PARALLEL_HOLD_TIME_MICROS);
-  delay(500); // TODO(aaron): Massive delay added for debugging -- remove.
+  delay(200); // TODO(aaron): Massive delay added for debugging -- remove.
 }
 
