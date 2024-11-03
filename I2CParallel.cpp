72cc70ef3501cc16935710f9b3e59285d27cd6d9
Aaron Kimball <akimball83@gmail.com>
2024-11-02 20:32:39 -0700

    Better state validation / err handling.
    * I/O functions do not actually invoke Wire if the init() fn has not
      been called first. They will gracefully operate as if I/O failed (they
      do not send or read a byte; they update the internal state of the
      driver, and return values reflecting 0 bytes actually read/written).
    * All output functions now return a size_t with the number of bytes
      written (1 on success, 0 on failure).
    * The input `getByte()` function now takes an optional `uint8_t &`
      reference argument that receives the number of bytes actually read,
      1 on success, 0 on failure.
    * Moved some internal constants to I2CParallel.cpp and out of the header
      where they clutter up the global namespace.
    * Implementations of convenience functions that really ought be inlined
      (e.g., `setAnd()`, `setOr()`, etc.) were moved into the class
      definition in the header.

--- I2CParallel.cpp
+++ I2CParallel.cpp
@@ -1,4 +1,4 @@
-// (C) Copyright 2021 Aaron Kimball
+// (C) Copyright 2021-2024 Aaron Kimball
 // This library is licensed under the terms of the BSD 3-Clause license.
 // See the accompanying LICENSE.txt file for full license text.
 //
@@ -7,23 +7,44 @@
 //
 // Include I2CParallel.h to use.
 
-#include<Arduino.h>
+#include <Arduino.h>
+
 #include "I2CParallel.h"
 
-static const uint8_t SEND_STOP = 1; // Always end our i2c transmissions with the STOP signal.
+// Always end our i2c transmissions with the STOP signal.
+static const uint8_t SEND_STOP = 1;
+
+// An "i2c address" that no PCF8574[A] can have. We use this to note that
+// the device driver has not been initialized, and disable I/O until this
+// condition is lifted.
+static const uint8_t UNINITIALIZED_I2C_ADDR = 0;
+
+// Use only the 7 less-significant bits of the address.
+// This will be left-shifted by 1 and a r/w flag bit appended as the lsb
+// for actual communication.
+static const uint8_t I2C_PARALLEL_ADDR_MASK = ((uint8_t)0x7F);
+
+// Timeout duration for I2C communications in microseconds; use 25 ms.
+static const uint32_t I2C_PARALLEL_WIRE_TIMEOUT = ((uint32_t)25000);
+
+// All bus lines begin at logic high.
+static const uint8_t I2C_PARALLEL_DEFAULT_BUS_STATE = I2C_PARALLEL_MAX_VAL;
 
 I2CParallel::I2CParallel() {
   _state = I2C_PARALLEL_DEFAULT_BUS_STATE;
+  // Start with 'invalid' i2caddr to confirm we are not initialized yet.
+  _i2cAddr = UNINITIALIZED_I2C_ADDR;
 }
 
 void I2CParallel::init(const uint8_t i2cAddr, const uint32_t busSpeed) {
   _i2cAddr = i2cAddr & I2C_PARALLEL_ADDR_MASK;
 
-  if (_i2cAddr < I2C_PCF8574_MIN_ADDR
-      || (_i2cAddr > I2C_PCF8574_MAX_ADDR && _i2cAddr < I2C_PCF8574A_MIN_ADDR)
-      || _i2cAddr > I2C_PCF8574A_MAX_ADDR) {
+  if (_i2cAddr < I2C_PCF8574_MIN_ADDR ||
+      (_i2cAddr > I2C_PCF8574_MAX_ADDR && _i2cAddr < I2C_PCF8574A_MIN_ADDR) ||
+      _i2cAddr > I2C_PCF8574A_MAX_ADDR) {
 
-    Serial.println(F("WARNING: I2C Parallel bus addr must be within 0x20..0x27 or 0x38..0x3F"));
+    Serial.println(F("WARNING: I2C Parallel bus addr must be within 0x20..0x27 "
+                     "or 0x38..0x3F"));
     Serial.print(F("Configured address: "));
     Serial.println(_i2cAddr, HEX);
   }
@@ -42,54 +63,32 @@ void I2CParallel::initInterrupt(const uint8_t digitalPinNum, void (*isr)()) {
   attachInterrupt(digitalPinToInterrupt(digitalPinNum), isr, FALLING);
 }
 
-void I2CParallel::setByte(const uint8_t val) {
-  Wire.beginTransmission(_i2cAddr);
-  Wire.write(val);
-  Wire.endTransmission(SEND_STOP);
+size_t I2CParallel::setByte(const uint8_t val) {
+  size_t numWritten = 0;
+  if (_i2cAddr != UNINITIALIZED_I2C_ADDR) {
+    // Only transmit if we have initialized i2c bus.
+    Wire.beginTransmission(_i2cAddr);
+    numWritten = Wire.write(val);
+    Wire.endTransmission(SEND_STOP);
+  }
   _state = val;
+  return numWritten;
 }
 
-void I2CParallel::write(const uint8_t val) {
-  setByte(val);
-}
-
-uint8_t I2CParallel::getByte() {
-  // Request 1 byte of data from the "read address" of the device (@ write_addr + 1)
-  uint8_t received = Wire.requestFrom((uint8_t)_i2cAddr, (uint8_t)1);
-  if (received != 1) {
-    _state = 0xFF;  // Unknown err state
-  } else {
-    _state = Wire.read();
+uint8_t I2CParallel::getByte(uint8_t &nBytesRead) {
+  nBytesRead = 0;
+  // Only actually perform I/O if I2C has been initialized.
+  if (_i2cAddr != UNINITIALIZED_I2C_ADDR) {
+    // Request 1 byte of data from the "read address" of the device
+    // (@ write_addr + 1)
+    nBytesRead = Wire.requestFrom((uint8_t)_i2cAddr, (uint8_t)1);
+    if (nBytesRead != 1) {
+      _state = 0xFF; // Unknown err state
+    } else {
+      _state = Wire.read();
+    }
   }
   return _state;
-}
-
-uint8_t I2CParallel::read() {
-  return getByte();
-}
-
-uint8_t I2CParallel::getLastState() const {
-  return _state;
-}
-
-void I2CParallel::enableInputs(const uint8_t mask) {
-  setOr(mask);
-}
-
-void I2CParallel::setOr(const uint8_t val) {
-  setByte(_state | val);
-}
-
-void I2CParallel::setAnd(const uint8_t val) {
-  setByte(_state & val);
-}
-
-void I2CParallel::setXor(const uint8_t val) {
-  setByte(_state ^ val);
-}
-
-void I2CParallel::increment() {
-  setByte((_state + 1) & I2C_PARALLEL_MAX_VAL);
 }
 
 void I2CParallel::waitForValid() {
@@ -98,6 +97,5 @@ void I2CParallel::waitForValid() {
   // in operational testing. (Despite being lower than the 4us requirement
   // specified on the datasheet.)
   //
-  //delayMicroseconds(I2C_PARALLEL_HOLD_TIME_MICROS);
+  // delayMicroseconds(I2C_PARALLEL_HOLD_TIME_MICROS);
 }
-
--- I2CParallel.h
+++ I2CParallel.h
@@ -1,4 +1,4 @@
-// (c) Copyright 2021 Aaron Kimball
+// (c) Copyright 2021-2024 Aaron Kimball
 // This library is licensed under the terms of the BSD 3-Clause license.
 // See the accompanying LICENSE.txt file for full license text.
 //
@@ -14,8 +14,11 @@
 #ifndef I2C_PARALLEL_H
 #define I2C_PARALLEL_H
 
-#include<inttypes.h>
-#include<Wire.h>
+#include <Arduino.h>
+
+#include <inttypes.h>
+
+#include <Wire.h>
 
 // The two chips are identical, except for their configurable address ranges:
 // PCF8574  address bits: 0 1 0 0 A2 A1 A0 0
@@ -29,92 +32,97 @@
 #define I2C_PCF8574A_MIN_ADDR ((uint8_t)0x38)
 #define I2C_PCF8574A_MAX_ADDR ((uint8_t)0x3F)
 
-// Use only the 7 less-significant bits of the address.
-// This will be left-shifted by 1 and a r/w flag bit appended as the lsb
-// for actual communication.
-#define I2C_PARALLEL_ADDR_MASK ((uint8_t)0x7F)
-
-#define I2C_SPEED_FAST     ((uint32_t)400000L)
+#define I2C_SPEED_FAST ((uint32_t)400000L)
 #define I2C_SPEED_STANDARD ((uint32_t)100000L)
-// The PCF8574 standard is specified at 100kHz but TI-manufactured chips are rated for 
-// 400kHz "fast mode" I2C. (See p.12 of https://www.ti.com/lit/an/scpa032/scpa032.pdf)
-// The pin-compatible NXP-mfr'd PCA8574 is also 400kHz device.
+// The PCF8574 standard is specified at 100kHz but TI-manufactured chips are
+// rated for 400kHz "fast mode" I2C. (See p.12 of
+// https://www.ti.com/lit/an/scpa032/scpa032.pdf) The pin-compatible NXP-mfr'd
+// PCA8574 is also 400kHz device.
 #ifndef I2C_PARALLEL_MAX_BUS_SPEED
 #define I2C_PARALLEL_MAX_BUS_SPEED I2C_SPEED_FAST
 #endif /* I2C_PARALLEL_MAX_BUS_SPEED */
 
-// Timeout duration for I2C communications in microseconds; use 25 ms.
-#define I2C_PARALLEL_WIRE_TIMEOUT ((uint32_t)25000)
-
+// This is an 8-bit unsigned int output device.
 #define I2C_PARALLEL_MAX_VAL ((uint8_t)0xFF)
 
-// All bus lines begin at logic high.
-#define I2C_PARALLEL_DEFAULT_BUS_STATE I2C_PARALLEL_MAX_VAL
+// bitmasks can refer to bits 0--7 in the output byte.
+#define I2C_MAX_BIT_POS ((uint8_t)7)
 
 // The time delay from I2C acknowledge until the output is valid.
-// (Also the hold time needed for driven inputs before the data can be reported back.)
+// (Also the hold time needed for driven inputs before the data can be reported
+// back.)
 #define I2C_PARALLEL_HOLD_TIME_MICROS ((unsigned int)4)
 
 class I2CParallel {
 public:
   I2CParallel();
-  ~I2CParallel() {};
+  ~I2CParallel(){};
 
   // Configure the 8-bit parallel bus with its expected 7-bit I2C address.
   // This must be within one of the two supported MIN_ADDR .. MAX_ADDR ranges.
-  void init(const uint8_t i2cAddr, const uint32_t busSpeed=I2C_PARALLEL_MAX_BUS_SPEED);
+  void init(const uint8_t i2cAddr,
+            const uint32_t busSpeed = I2C_PARALLEL_MAX_BUS_SPEED);
 
-  // Configure the specified pin as the recipient of the INT_L signal from 
+  // Configure the specified pin as the recipient of the INT_L signal from
   // the I2C parallel bus. The specified isr method will be called when INT_L
-  // is pulled low by the PCF8574. Add a pull-up between this pin and Vcc. 
+  // is pulled low by the PCF8574. Add a pull-up between this pin and Vcc.
   void initInterrupt(const uint8_t digitalPinNum, void (*isr)());
 
   // Set the value to emit on the 8-bit bus. This value is latched and held
-  // until overwritten by another setByte() or driven by the other side of the bus.
-  void setByte(const uint8_t val);
-  void write(const uint8_t val); // synonym for setByte().
+  // until overwritten by another setByte() or driven by the other side of the
+  // bus. Returns the number of bytes written (1 on success, 0 on failure).
+  size_t setByte(const uint8_t val);
+  // Synonym for setByte().
+  size_t write(const uint8_t val) { return setByte(val); };
 
   // Read back the current contents of the 8-bit bus. The bus can be driven by
   // connected logic on any lines that we have set to HIGH via setByte().
   // On power-up, all data lines initialize to high (getByte() == 0xFF).
-  uint8_t getByte();
-  uint8_t read(); // synonym for getByte().
+  // This returns the value received and sets nBytesRead to 1 or 0 depending on
+  // whether or not it has successfully performed the I/O read. If bytesRead is
+  // 0, the output should not be used.
+  uint8_t getByte(uint8_t &nBytesRead);
+  uint8_t getByte() {
+    uint8_t numReceived = 0;
+    return getByte(numReceived);
+  };
+  uint8_t read() { return getByte(); }; // synonym for getByte().
 
-  // Read back the last known contents of the bus without actually reading over i2c.
-  uint8_t getLastState() const;
+  // Read back the last known contents of the bus without actually reading over
+  // i2c.
+  uint8_t getLastState() const { return _state; };
 
   // Configure some data lines as inputs according to the specified mask.  The
   // masked data lines will be allowed to pull up to logic HIGH and can then be
   // driven by the connected device(s). Subsequently writing a logic LOW to any
   // bits with setByte() will drive those lines low and disable input mode.
-  void enableInputs(const uint8_t mask);
+  void enableInputs(const uint8_t mask) { setOr(mask); };
 
   // Apply a bitwise operation to the current bus state.
-  void setOr(const uint8_t val);
-  void setAnd(const uint8_t val);
-  void setXor(const uint8_t val);
-
+  size_t setOr(const uint8_t val) { return setByte(_state | val); };
+  size_t setAnd(const uint8_t val) { return setByte(_state & val); };
+  size_t setXor(const uint8_t val) { return setByte(_state ^ val); };
 
   /** Set the specified bit (0--7) high. */
-  void setBit(const uint8_t bitPos) {
-    if (bitPos > 7) {
-      return; /* Nothing to do. */
+  size_t setBit(const uint8_t bitPos) {
+    if (bitPos > I2C_MAX_BIT_POS) {
+      return 0; /* Nothing to do. */
     }
     uint8_t mask = 1 << bitPos;
-    setOr(mask);
+    return setOr(mask);
   }
 
   /** Set the specified bit (0--7) low. */
-  void clrBit(const uint8_t bitPos) {
-    if (bitPos > 7) {
-      return; /* Nothing to do. */
+  size_t clrBit(const uint8_t bitPos) {
+    if (bitPos > I2C_MAX_BIT_POS) {
+      return 0; /* Nothing to do. */
     }
     uint8_t mask = ~(1 << bitPos);
-    setAnd(mask);
+    return setAnd(mask);
   }
 
   // Increment the bus arithmetically by 1; 0xFF + 1 rolls back to 0.
-  void increment();
+  size_t increment() { return setByte((_state + 1) & I2C_PARALLEL_MAX_VAL); };
 
   // Delay until the transmitted data is ready on the parallel bus,
   // or delay until parallel bus inputs can be queried.
@@ -122,10 +130,7 @@ public:
 
 private:
   uint8_t _i2cAddr; // Address of chip on the I2C bus.
-  uint8_t _state; // State of the 8 data lines.
+  uint8_t _state;   // State of the 8 data lines.
 };
 
-
-
 #endif /* I2C_PARALLEL_H */
-
